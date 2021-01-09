ebb8af10678fb8e81533ddbc7bfe752e7881fc25
Olaide Alaka <olaidealaka12@gmail.com>
2021-01-09 19:12:19 +0100

    configs2

--- inventory/settings.py
+++ inventory/settings.py
@@ -30,6 +30,14 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+#messaging
+DEFAULT_FROM_EMAIL = my_secret.EMAIL_HOST_USER
+SERVER_EMAIL = my_secret.EMAIL_HOST_USER
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = my_secret.EMAIL_HOST_USER
+EMAIL_HOST_PASSWORD = my_secret.EMAIL_HOST_PASSWORD
+EMAIL_PORT = 587
 
 # Application definition
 
